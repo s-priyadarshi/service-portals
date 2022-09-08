ad7578bccee8adff7316c3f2aac2d4d5f3c020d8
Sanket Priyadarshi <51432182+s-priyadarshi@users.noreply.github.com>
2022-09-08 12:48:55 +0530

    Update insert-images.js

--- insert-images.js
+++ insert-images.js
@@ -1,9 +1,14 @@
 var settings = {
-  "url": "https://api.github.com/repos/s-priyadarshi/service-portals/contents/images",
-  "method": "GET",
-  "timeout": 0,
+   "url": "https://api.github.com/repos/s-priyadarshi/service-portals/contents/images",
+   "method": "GET",
+   "timeout": 0,
 };
 
 $.ajax(settings).done(function (response) {
-  console.log(response);
+   response.forEach(function (image) {
+      var img = document.createElement('img');
+      img.src = image.path;
+      document.querySelector(".images.container").appendChild(img);
+   });
+   console.log(response);
 });
